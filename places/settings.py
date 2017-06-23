45b2b4927711f554aef7b5ba9f1bfe836b4836a5
tirthankar <tirthankar.banerjee@sky-bits.com>
2017-06-23 21:47:27 +0530

    2306 2147

--- places/settings.py
+++ places/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-from keras.models import model_from_json
+import keras
+#from keras.models import model_from_json
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
